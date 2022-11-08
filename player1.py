01a827c8c7de523c570f7315a1a0d9d1ba0632ed
Pinecone-Programs <116664595+Pinecone-Programs@users.noreply.github.com>
2022-11-08 09:34:23 -0500

    Player module added, animation

---
+++ player1.py
@@ -0,0 +1,11 @@
+import pygame, sys, main
+
+
+p1StandIdleAnim = [pygame.image.load("CanGooseAnimations/frame0000.png"),pygame.image.load("CanGooseAnimations/frame0001.png")]
+p1StandWalkAnim = [pygame.image.load("CanGooseAnimations/frame0001.png"),pygame.image.load("CanGooseAnimations/frame0002.png")]
+p1StandAttackAnim = [pygame.image.load("CanGooseAnimations/frame0002.png"),pygame.image.load("CanGooseAnimations/frame0003.png")]
+p1CrouchIdleAnim = [pygame.image.load("CanGooseAnimations/frame0004.png")]
+p1CrouchWalkAnim = [pygame.image.load("CanGooseAnimations/frame0004.png"),pygame.image.load("CanGooseAnimations/frame0005.png")]
+p1CrouchAttackAnim = [pygame.image.load("CanGooseAnimations/frame0006.png"),pygame.image.load("CanGooseAnimations/frame0007.png")]
+p1JumpIdleAnim = [pygame.image.load("CanGooseAnimations/frame0008.png"),pygame.image.load("CanGooseAnimations/frame0009.png")]
+p1JumpAttackAnim = [pygame.image.load("CanGooseAnimations/frame0010.png")]
