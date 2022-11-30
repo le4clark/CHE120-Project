e1c1fa403138709ee13087f3599e778736963573
Pinecone-Programs <116664595+Pinecone-Programs@users.noreply.github.com>
2022-11-29 21:52:50 -0500

    player2 animations added

--- player.py
+++ player.py
@@ -19,3 +19,22 @@ class Player1:
       #centers it and places at location ~MA
       imagerec.center = center
       self.pos = imagerec.move(-300, 200)
+class Player2:
+   def __init__(self, center):
+
+      #declares all the animations for the player ~MA
+      self.p2StandIdleAnim = [pygame.image.load("WhiGooseAnimations/frame0000.png"),pygame.image.load("WhiGooseAnimations/frame0001.png")]
+      self.p2StandWalkAnim = [pygame.image.load("WhiGooseAnimations/frame0001.png"),pygame.image.load("WhiGooseAnimations/frame0002.png")]
+      self.p2StandAttackAnim = [pygame.image.load("WhiGooseAnimations/frame0002.png"),pygame.image.load("WhiGooseAnimations/frame0003.png")]
+      self.p2CrouchIdleAnim = [pygame.image.load("WhiGooseAnimations/frame0004.png")]
+      self.p2CrouchWalkAnim = [pygame.image.load("WhiGooseAnimations/frame0004.png"),pygame.image.load("WhiGooseAnimations/frame0005.png")]
+      self.p2CrouchAttackAnim = [pygame.image.load("WhiGooseAnimations/frame0006.png"),pygame.image.load("WhiGooseAnimations/frame0007.png")]
+      self.p2JumpIdleAnim = [pygame.image.load("WhiGooseAnimations/frame0008.png"),pygame.image.load("WhiGooseAnimations/frame0009.png")]
+      self.p2JumpAttackAnim = [pygame.image.load("WhiGooseAnimations/frame0010.png")]
+      #temp just one image ~MA
+      self.image = self.p2StandIdleAnim[0]
+      #gets image rect ~MA
+      imagerec = self.image.get_rect()
+      #centers it and places at location ~MA
+      imagerec.center = center
+      self.pos = imagerec.move(-300, 200)
